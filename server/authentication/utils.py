26394515483aa5789cd77ca5f441f2bae5390c59
Your Name <deysoumyajyoti2017@gmail.com>
2021-10-12 20:46:03 +0530

    changing the environment variables

--- server/authentication/utils.py
+++ server/authentication/utils.py
@@ -8,16 +8,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 serviceAccountKey = {
-    "type": os.environ.get('type'),
-    "project_id": os.environ.get('project_id'),
-    "private_key_id": os.environ.get('private_key_id'),
-    "private_key": os.environ.get('private_key').replace('\\n', '\n'),
-    "client_email": os.environ.get('client_email'),
-    "client_id": os.environ.get('client_id'),
-    "auth_uri": os.environ.get('auth_uri'),
-    "token_uri": os.environ.get('token_uri'),
-    "auth_provider_x509_cert_url": os.environ.get('auth_provider_x509_cert_url'),
-    "client_x509_cert_url": os.environ.get('client_x509_cert_url'),
+    "type": os.environ.get('TYPE'),
+    "project_id": os.environ.get('PROJECT_ID'),
+    "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
+    "private_key": os.environ.get('PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.environ.get('CLIENT_EMAIL'),
+    "client_id": os.environ.get('CLIENT_ID'),
+    "auth_uri": os.environ.get('AUTH_URI'),
+    "token_uri": os.environ.get('TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.environ.get('AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.environ.get('CLIENT_X509_CERT_URL'),
 }
 
 cred = credentials.Certificate(serviceAccountKey)
