a33e73c63668d2af1d1bea05956f8e5c36f58190
Your Name <deysoumyajyoti2017@gmail.com>
2021-10-15 20:27:12 +0530

    changing repo architecture

--- server/authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import LoginView, ProfileView
-
-urlpatterns = [
-    path('login/', view=LoginView.as_view()),
-    path('profile/', view=ProfileView.as_view()),
-]
